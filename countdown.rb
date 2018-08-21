b1cdb573f1ed4362f6c965db88387e6118843a29
Amalie S <amaliesmidth@gmail.com>
2018-08-21 16:06:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,16 @@
+# def countdown(int)
+#   i = int
+#   while i > 0 do
+#     puts "#{i} SECOND(S)!"
+#     i-=1
+#   end
+#   return 'HAPPY NEW YEAR!'
+# end
+
 def countdown(int)
-  i = int
-  while i > 0 do
-    puts "#{i} SECOND(S)!"
-    i-=1
+  while int > 0 do
+    puts "#{int} SECOND(S)!"
+    int -= 1
   end
   return 'HAPPY NEW YEAR!'
 end
